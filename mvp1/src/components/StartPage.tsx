d16678cdb90b22b4ce0c10dde9463aded69ef74f
raigal-r <raquel.raigal@outlook.es>
2023-08-31 15:20:31 +0200

    initial-page

--- mvp1/src/components/StartPage.tsx
+++ mvp1/src/components/StartPage.tsx
@@ -14,7 +14,7 @@ export default function StartPage () {
                     <img src="./192.png"/>
                     <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl max-h-[64px] mb-4">
                         PEERUP
-                    </h1>∫
+                    </h1>
                     <div className="w-[75vw]">
                         <p className="text-xl text-center tracking-tight text-white sm:text-xl md:text-xl lg:text-xl xl:text-xl mb-20">
                             Welcome in PEERUP the decentralized financial management with chat for teams and small communities  
