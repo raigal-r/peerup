3ffee8b92956b4edd04f546e57361f682566c769
raigal-r <raquel.raigal@outlook.es>
2023-08-31 22:34:22 +0200

    start-to-home

--- mvp1/src/components/TeamFooter.tsx
+++ mvp1/src/components/TeamFooter.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
 import homeIcon from "./home-icon.svg"
-import chatIcon from "./home-icon.svg"
+import chatIcon from "./chat-icon.svg"
 import treasuryIcon from "./treasury-icon.svg"
 import contactsIcon from "./contacts-icon.svg"
 
 export default function TeamFooter () {
     return(
-        <section className="MyApp">
-            <main>
+        <section>
+            <main className="MyApp">
                 <div>
                     <div className="container-fluid min-h-[10px] flex flex-grow justify-between bg-[#0097b3] border-opacity-20 border-solid border-2 border-gray-100 rounded-xl">
                         <div className="flex flex-grow justify-around items-center ml-4 mt-4 mb-4">
