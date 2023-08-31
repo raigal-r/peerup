b645161921ad743dadfe2eeee2851308e2cd2414
raigal-r <raquel.raigal@outlook.es>
2023-08-31 15:14:11 +0200

    initial-page

--- mvp1/src/components/StartPage.tsx
+++ mvp1/src/components/StartPage.tsx
@@ -12,7 +12,7 @@ export default function StartPage () {
             <main className="flex min-h-screen flex-col items-center justify-around bg-[#0097b3]">
                 <div className="flex flex-col items-center justify-center" >
                     <img src="./192.png"/>
-                    <h1 className="text-2xl mb-10 font-extrabold tracking-tight text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+                    <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl max-h-[64px]">
                         PEERUP
                     </h1>
                     <div className="w-[75vw]">
