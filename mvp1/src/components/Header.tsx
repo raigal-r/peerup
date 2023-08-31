cfb1f3a7858cb2ca5b6e95114f85127ae807a812
raigal-r <raquel.raigal@outlook.es>
2023-08-31 11:44:12 +0200

    header v1

--- mvp1/src/components/Header.tsx
+++ mvp1/src/components/Header.tsx
@@ -9,16 +9,18 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 export default function Header () {
     return(
         <section>
-            <main className="flex flex-col items-center justify-center bg-[#0097b3]">
-                <div className="container  min-h-[60px] max-h-[120px] flex flex-grow items-start justify-between gap-12 px-4 py-4 sm:py-8 md:py-12 lg:py-16 xl:py-20">
-                    <div className="flex">
-                        <img src="./192.png" className="max-h-[64px] items-center" alt="logo"/>
+            <main>
+                <div className="container-fluid min-h-[10px] flex flex-grow justify-between bg-[#0097b3]">
+                    <div className="flex justify-center items-center">
+                        <img src="./192.png" className="max-w-[20%]" alt="logo"/>
                         <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl max-h-[64px]">
                             PEERUP
                         </h1>
                     </div>
-                    <div className="items-center">
-                        <ConnectButton />
+                    <div className="flex justify-center items-center mr-4">
+                        <div >
+                            <ConnectButton />
+                        </div>  
                     </div>
                 </div>
             </main>
