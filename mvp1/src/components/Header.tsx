a4d0847b7ec7e661c19bc89e5be705f37aefe8f4
raigal-r <raquel.raigal@outlook.es>
2023-08-31 15:13:36 +0200

    initial-page

--- mvp1/src/components/Header.tsx
+++ mvp1/src/components/Header.tsx
@@ -10,10 +10,10 @@ export default function Header () {
         <section>
             <main>
                 <div className="bg-[#0097b3]">
-                    <div className="container-fluid min-h-[10px] flex flex-grow justify-between bg-[#0097b3] border-opacity-20 border-solid border-2 border-gray-100 rounded-xl pr-1 pl-1">
+                    <div className="container-fluid min-h-[10px] flex flex-grow justify-between bg-[#0097b3] border-opacity-20 border-solid border-2 border-gray-100 rounded-xl">
                         <div className="flex justify-start items-center ml-4 mt-4 mb-4">
-                            <img src="./192.png" className="max-w-[20%]" alt="logo"/>
-                            <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl max-h-[64px]">
+                            <img src="./192.png" className="max-w-[15%]" alt="logo"/>
+                            <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-4xl md:text-4xl lg:text-4xl xl:text-5xl max-h-[64px]">
                                 PEERUP
                             </h1>
                         </div>
