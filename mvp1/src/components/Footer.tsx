55101976d7b37e01e7ec82de100e50c6319df076
raigal-r <raquel.raigal@outlook.es>
2023-08-31 22:42:57 +0200

    start-to-home

--- mvp1/src/components/Footer.tsx
+++ mvp1/src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
                     <div className="container-fluid min-h-[10px] flex flex-grow justify-between bg-[#0097b3] border-opacity-20 border-solid border-2 border-gray-100 rounded-xl">
                         <div className="flex flex-grow justify-around items-center ml-4 mt-4 mb-4">
                             <div>
-                                <Image src={homeIcon} className="max-w-[100%]" alt="home-icon"/>
+                                <Image src="homeIcon" className="max-w-[100%]" alt="home-icon"/>
                             </div>
                             <div>
                                 <Image src={chatIcon} className="max-w-[100%]" alt="chat-icon"/>  
