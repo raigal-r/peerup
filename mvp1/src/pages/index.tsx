774f8da95673f219598bf3ca19df72176cdeae80
raigal-r <raquel.raigal@outlook.es>
2023-08-31 21:43:02 +0200

    start-to-home

--- mvp1/src/pages/index.tsx
+++ mvp1/src/pages/index.tsx
@@ -37,17 +37,6 @@ const Index = () => {
 
   return (
     <>
-      <Head>
-        <title>PEERUP</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        {/* import manifest for the PWA configuration*/}
-        <link rel="manifest" href="/manifest.json" />
-        {/* import manifest for the Roboto font family*/}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Orbitron&family=Roboto:wght@100&display=swap" rel="stylesheet" />
-      </Head>
       <Suspense fallback={<div>Loading...</div>}>
         <AppDynamic />
       </Suspense>
